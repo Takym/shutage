849713eaf31826d3bf6b862b4af8b67039f943e9
Takym <15681312+Takym@users.noreply.github.com>
2020-11-10 23:02:12 +0900

    charname.js更新

--- assets/scripts/charname.js
+++ assets/scripts/charname.js
@@ -1,150 +1,172 @@
+const okabe    = "#663030;"; // 岡部
+const mayuri   = "#5566EE;"; // まゆり
+const daru     = "#BBCC22;"; // ダル
+const tina     = "#EE5555;"; // 紅莉栖
+const moeka    = "#EAC220;"; // 桐生萌郁
+const ruka     = ""; // ルカ子
+const faris    = ""; // ﾌｪｲﾘｽ
+const suzu     = ""; // 鈴羽
+const maho     = ""; // 真帆
+const kagari   = ""; // かがり
+const yuki     = ""; // 由季
+const brown    = ""; // 天王寺
+const nae      = ""; // 綯
+const leskinen = ""; // レスキネン
+// 朱美, フブキ, カエデ, ｼﾝｲﾁ, 中鉢, 栄輔, 幸高, ちかね, 黒木, 4℃, 伽夜乃, 今宮綴, レイエス, 井崎
+
 window.onload = function() {
 	var tags = document.getElementsByTagName("strong");
 	for (var i = 0; i < tags.length; ++i) {
 		if (tags[i].innerText == "^岡部倫太郎") {
 			tags[i].innerText = "岡部倫太郎";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部") {
 			tags[i].innerText = "岡部";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^倫太郎") {
 			tags[i].innerText = "倫太郎";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^鳳凰院凶真") {
 			tags[i].innerText = "鳳凰院凶真";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^鳳凰院") {
 			tags[i].innerText = "鳳凰院";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^凶真") {
 			tags[i].innerText = "凶真";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^未来岡部") {
 			tags[i].innerText = "未来岡部";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^現在岡部") {
 			tags[i].innerText = "現在岡部";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^過去岡部") {
 			tags[i].innerText = "過去岡部";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部α") {
 			tags[i].innerText = "岡部α";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部β") {
 			tags[i].innerText = "岡部β";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部SG") {
 			tags[i].innerText = "岡部SG";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部γ") {
 			tags[i].innerText = "岡部γ";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部δ") {
 			tags[i].innerText = "岡部δ";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部ε") {
 			tags[i].innerText = "岡部ε";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^岡部Ω") {
 			tags[i].innerText = "岡部Ω";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^オカリン") {
 			tags[i].innerText = "オカリン";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^執念オカリン") {
 			tags[i].innerText = "執念オカリン";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^絶倫オカリン") {
 			tags[i].innerText = "絶倫オカリン";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^鳳凰院クロト") {
 			tags[i].innerText = "鳳凰院クロト";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^クロト") {
 			tags[i].innerText = "クロト";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^M3") {
 			tags[i].innerText = "M3";
-			tags[i].style.cssText = "font-weight: bold; color: #663030;";
+			tags[i].style.cssText = "font-weight: bold; color: " + okabe;
 		} else if (tags[i].innerText == "^椎名まゆり") {
 			tags[i].innerText = "椎名まゆり";
-			tags[i].style.cssText = "font-weight: bold; color: #5566EE;";
+			tags[i].style.cssText = "font-weight: bold; color: " + mayuri;
 		} else if (tags[i].innerText == "^椎名") {
 			tags[i].innerText = "椎名";
-			tags[i].style.cssText = "font-weight: bold; color: #5566EE;";
+			tags[i].style.cssText = "font-weight: bold; color: " + mayuri;
 		} else if (tags[i].innerText == "^まゆり") {
 			tags[i].innerText = "まゆり";
-			tags[i].style.cssText = "font-weight: bold; color: #5566EE;";
+			tags[i].style.cssText = "font-weight: bold; color: " + mayuri;
 		} else if (tags[i].innerText == "^まちょり") {
 			tags[i].innerText = "まちょり";
-			tags[i].style.cssText = "font-weight: bold; color: #5566EE;";
+			tags[i].style.cssText = "font-weight: bold; color: " + mayuri;
 		} else if (tags[i].innerText == "^まちょしぃ") {
 			tags[i].innerText = "まちょしぃ";
-			tags[i].style.cssText = "font-weight: bold; color: #5566EE;";
+			tags[i].style.cssText = "font-weight: bold; color: " + mayuri;
 		} else if (tags[i].innerText == "^まっちょしぃ") {
 			tags[i].innerText = "まっちょしぃ";
-			tags[i].style.cssText = "font-weight: bold; color: #5566EE;";
+			tags[i].style.cssText = "font-weight: bold; color: " + mayuri;
 		} else if (tags[i].innerText == "^橋田至") {
 			tags[i].innerText = "橋田至";
-			tags[i].style.cssText = "font-weight: bold; color: #BBCC22;";
+			tags[i].style.cssText = "font-weight: bold; color: " + daru;
 		} else if (tags[i].innerText == "^橋田") {
 			tags[i].innerText = "橋田";
-			tags[i].style.cssText = "font-weight: bold; color: #BBCC22;";
+			tags[i].style.cssText = "font-weight: bold; color: " + daru;
 		} else if (tags[i].innerText == "^至") {
 			tags[i].innerText = "至";
-			tags[i].style.cssText = "font-weight: bold; color: #BBCC22;";
+			tags[i].style.cssText = "font-weight: bold; color: " + daru;
 		} else if (tags[i].innerText == "^ダル") {
 			tags[i].innerText = "ダル";
-			tags[i].style.cssText = "font-weight: bold; color: #BBCC22;";
+			tags[i].style.cssText = "font-weight: bold; color: " + daru;
 		} else if (tags[i].innerText == "^DaSH") {
 			tags[i].innerText = "DaSH";
-			tags[i].style.cssText = "font-weight: bold; color: #BBCC22;";
+			tags[i].style.cssText = "font-weight: bold; color: " + daru;
 		} else if (tags[i].innerText == "^バレル・タイター") {
 			tags[i].innerText = "バレル・タイター";
-			tags[i].style.cssText = "font-weight: bold; color: #BBCC22;";
+			tags[i].style.cssText = "font-weight: bold; color: " + daru;
 		} else if (tags[i].innerText == "^牧瀬紅莉栖") {
 			tags[i].innerText = "牧瀬紅莉栖";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^牧瀬") {
 			tags[i].innerText = "牧瀬";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^紅莉栖") {
 			tags[i].innerText = "紅莉栖";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^助手") {
 			tags[i].innerText = "助手";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^クリスティーナ") {
 			tags[i].innerText = "クリスティーナ";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^ティーナ") {
 			tags[i].innerText = "ティーナ";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^セレセブ") {
 			tags[i].innerText = "セレセブ";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^栗悟飯とカメハメ波") {
 			tags[i].innerText = "栗悟飯とカメハメ波";
-			tags[i].style.cssText = "font-weight: bold; color: #EE5555;";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
+		} else if (tags[i].innerText == "^Ama紅莉栖") {
+			tags[i].innerText = "Ama紅莉栖";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
+		} else if (tags[i].innerText == "^甘栗") {
+			tags[i].innerText = "甘栗";
+			tags[i].style.cssText = "font-weight: bold; color: " + tina;
 		} else if (tags[i].innerText == "^桐生萌郁") {
 			tags[i].innerText = "桐生萌郁";
-			tags[i].style.cssText = "font-weight: bold; color: #EAC220;";
+			tags[i].style.cssText = "font-weight: bold; color: " + moeka;
 		} else if (tags[i].innerText == "^桐生") {
 			tags[i].innerText = "桐生";
-			tags[i].style.cssText = "font-weight: bold; color: #EAC220;";
+			tags[i].style.cssText = "font-weight: bold; color: " + moeka;
 		} else if (tags[i].innerText == "^萌郁") {
 			tags[i].innerText = "萌郁";
-			tags[i].style.cssText = "font-weight: bold; color: #EAC220;";
+			tags[i].style.cssText = "font-weight: bold; color: " + moeka;
 		} else if (tags[i].innerText == "^閃光の指圧師") {
 			tags[i].innerText = "閃光の指圧師";
-			tags[i].style.cssText = "font-weight: bold; color: #EAC220;";
+			tags[i].style.cssText = "font-weight: bold; color: " + moeka;
 		} else if (tags[i].innerText == "^指圧師") {
 			tags[i].innerText = "指圧師";
-			tags[i].style.cssText = "font-weight: bold; color: #EAC220;";
+			tags[i].style.cssText = "font-weight: bold; color: " + moeka;
 		} else if (tags[i].innerText == "^M4") {
 			tags[i].innerText = "M4";
-			tags[i].style.cssText = "font-weight: bold; color: #EAC220;";
+			tags[i].style.cssText = "font-weight: bold; color: " + moeka;
 		}
 	}
 }
